33358ec93f6e808d8b13cb2b59b2979ea35e025b
Shwinn <ashwingupta98@gmail.com>
2018-03-18 02:10:45 -0500

    starting wifi direct chat communication

--- app/src/main/java/com/example/multiprotocoldatatransfer/MainActivity.java
+++ app/src/main/java/com/example/multiprotocoldatatransfer/MainActivity.java
@@ -2,16 +2,14 @@ package com.example.multiprotocoldatatransfer;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothServerSocket;
-import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.provider.Settings;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,22 +17,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.app.Fragment;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.lang.ref.WeakReference;
-import java.net.Socket;
 import java.util.Set;
-import java.util.UUID;
 
-import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_ENABLE_BT = 1;
     protected static BluetoothAdapter mBluetoothAdapter;
     private BluetoothDevice deviceDiscovered;
     protected static ConnectedThread deviceConnection = new ConnectedThread();
+    private final IntentFilter bluetoothIntentFilter = new IntentFilter();
+
+    Channel mChannel;
+    WifiP2pManager mManager;
+    boolean isWifiP2PEnabled = true;
+    private final IntentFilter wifiDirectIntentFilter = new IntentFilter();
+    BroadcastReceiver wifiDirectReceiver;
 
     private Button serverButton;
     private Button clientButton;
@@ -46,9 +45,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        serverButton = findViewById(R.id.button2);
+        clientButton = findViewById(R.id.button3);
+        sendButton = findViewById(R.id.button);
+        typedMessage = findViewById(R.id.editText);
+        chat = findViewById(R.id.textView);
+
+        chat.append("\n");
+
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
-            System.out.println("Device does not support Bluetooth");
+            Log.e("Printing", "Device does not support Bluetooth");
         }
         else {
             if (!mBluetoothAdapter.isEnabled()) {
@@ -72,22 +80,14 @@ public class MainActivity extends AppCompatActivity {
                 break;
             }
 
-            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-            registerReceiver(mReceiver, filter);
+            //IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            bluetoothIntentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(mReceiver, bluetoothIntentFilter);
 
             Intent discoverableIntent =
                     new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
             discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
             startActivity(discoverableIntent);
-
-            setContentView(R.layout.activity_main);
-            serverButton = findViewById(R.id.button2);
-            clientButton = findViewById(R.id.button3);
-            sendButton = findViewById(R.id.button);
-            typedMessage = findViewById(R.id.editText);
-            chat = findViewById(R.id.textView);
-
-            chat.append("\n");
 
             serverButton.setOnClickListener(new View.OnClickListener(){
                 public void onClick(View v){
@@ -116,6 +116,38 @@ public class MainActivity extends AppCompatActivity {
             mBluetoothAdapter.startDiscovery();
             Log.e("Printing", "Started device discovery");
         }
+
+        wifiDirectIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        wifiDirectIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        wifiDirectIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        wifiDirectIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mManager = (WifiP2pManager)getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        wifiDirectReceiver = new WifiDirectBroadcastReceiver(mManager, mChannel, this);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        registerReceiver(wifiDirectReceiver, wifiDirectIntentFilter);
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Log.e("Printing", "Wifi Direct Discovery Succeeded");
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+                Log.e("Printing", "Wifi Direct Discovery Failed with code: " + reasonCode);
+            }
+        });
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        unregisterReceiver(wifiDirectReceiver);
     }
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
@@ -130,6 +162,18 @@ public class MainActivity extends AppCompatActivity {
                 String deviceHardwareAddress = device.getAddress(); // MAC address
                 Log.e("Printing", "BROADCAST RECEIVER DISCOVERED A DEVICE");
                 Log.e("Printing", "Device Name: " + deviceName + " Device Hardware Address: " + deviceHardwareAddress);
+            }
+            else if(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)){
+
+            }
+            else if(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)){
+
+            }
+            else if(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)){
+
+            }
+            else if(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)){
+
             }
         }
     };
